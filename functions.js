c14de6c28823a379378da8af8d5baa5c76dd79f5
Nerine <nerinelaw@gmail.com>
2019-05-20 09:57:31 -0700

    Added JSON parse

--- functions.js
+++ functions.js
@@ -1,6 +1,25 @@
 var message;
 var messageList = [];
+var name;
+var myJSON = '{"welcome-message":"Welcome", "random":"Leave a message!"}';
+var myObj;
 
+function loadText(){
+    name = document.getElementById("name-input");
+    myObj = JSON.parse(myJSON);
+    messageList.push(myObj.welcome-message + name + "!");
+    openChatPage();
+}
+
+function loadMessages(){
+    myMsg = JSON.parse(myJSON);
+    messageList.push(myMsg.random);
+    document.getElementById("see-message").value = messageList.join("\n\n");
+}
+
+function enterChat(){
+    openChatPage();
+}
 
 function purifyWord(phrase){
     var checkBox = document.getElementById("pg13");
@@ -22,7 +41,12 @@ function addMessage(){
 }
 
 function clearInputText(){
+    document.getElementById("text-input").value = "";
+}
 
+function clearMessages(){
+    messageList = [];
+    document.getElementById("see-message").value = messageList;
 }
 
 function openWelcome(){
